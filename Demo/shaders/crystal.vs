d4b99533edec3441d47e7bf4051612b895297db2
LITON BARMAN <53089204+Liton-Barman@users.noreply.github.com>
2021-11-24 19:45:54 +0530

    Add files via upload
    Added demo

---
+++ Demo/shaders/crystal.vs
@@ -0,0 +1,33 @@
+#version 330 core
+
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aNormal;
+layout (location = 2) in vec2 aTexCoords;
+
+out vec2 TexCoords;
+
+out VS_OUT {
+    vec3 FragPos;
+    vec3 Normal;
+    vec2 TexCoords;
+    vec4 FragPosLightSpace;
+} vs_out;
+
+uniform vec3 colorP;
+
+out vec3 colorF;
+
+uniform mat4 projection;
+uniform mat4 view;
+uniform mat4 model;
+uniform mat4 lightSpaceMatrix;
+
+void main()
+{   
+    colorF = colorP;
+    vs_out.FragPos = vec3(model * vec4(aPos, 1.0));
+    vs_out.Normal = transpose(inverse(mat3(model))) * aNormal;
+    vs_out.TexCoords = aTexCoords;
+    vs_out.FragPosLightSpace = lightSpaceMatrix * vec4(vs_out.FragPos, 1.0);
+    gl_Position = projection * view * model * vec4(aPos, 1.0);
+}
---
+++ Demo/shaders/model.fs
@@ -0,0 +1,86 @@
+// mY version of shadow mapping with perfect result
+
+#version 330 core
+
+out vec4 FragColor;
+
+in VS_OUT {
+    vec3 FragPos;
+    vec3 Normal;
+    vec2 TexCoords;
+    vec4 FragPosLightSpace;
+} fs_in;
+
+in vec3 colorF;
+
+uniform sampler2D diffuseTexture;
+uniform sampler2D shadowMap;
+
+uniform vec3 lightPos;
+uniform vec3 viewPos;
+
+float ShadowCalculation(vec4 fragPosLightSpace){
+   
+    vec3 projCoords = fragPosLightSpace.xyz / fragPosLightSpace.w;
+    projCoords = projCoords * 0.5 + 0.5;       // 0.5   0.5
+   
+
+    float closestDepth = texture(shadowMap, projCoords.xy).r; 
+    float currentDepth = projCoords.z;
+   
+    vec3 normal = normalize(fs_in.Normal);
+    vec3 lightDir = normalize(lightPos - fs_in.FragPos);
+ 
+    float bias = max(0.05 * (1.0 - dot(normal, lightDir)), 0.005);
+    float shadow = 0.0;
+ 
+    vec2 texelSize = 1.0 / textureSize(shadowMap, 0);
+   
+   for(int x = -1; x <= 1; ++x)
+    {
+        for(int y = -1; y <= 1; ++y)
+        {
+            float pcfDepth = texture(shadowMap, projCoords.xy + vec2(x, y) * texelSize).r; 
+            shadow += currentDepth - bias > pcfDepth  ? 1.0 : 0.0;        
+        }    
+    }
+    shadow /= 9.0;
+    
+    
+	
+    return shadow;
+}
+
+void main()
+{           
+    vec3 color = colorF ;//texture(diffuseTexture, fs_in.TexCoords).rgb;
+    	
+	vec3 normal = normalize(fs_in.Normal);
+    vec3 lightColor = vec3(1.0, 0.996, 0.882);
+	
+    vec3 ambient = 0.2 * color;
+    // diffuse
+
+    vec3 lightDir = normalize(lightPos - fs_in.FragPos);
+    float diff = max(dot(lightDir, normal), 0.0);
+    vec3 diffuse = diff * lightColor;
+    // specular
+    vec3 viewDir = normalize(viewPos - fs_in.FragPos);
+    vec3 reflectDir = reflect(-lightDir, normal);
+
+    float spec = 0.0;
+    vec3 halfwayDir = normalize(lightDir + viewDir);  
+    spec = pow(max(dot(normal, halfwayDir), 0.0), 64.0);
+    vec3 specular = spec * lightColor;    
+    // calculate shadow
+    float shadow = ShadowCalculation(fs_in.FragPosLightSpace);                      
+    //vec3 lighting = (ambient + (1.0 - shadow) * (diffuse + specular)) * color * 0.8;    
+    vec3 lighting = (ambient + (1.0 - shadow) * (diffuse + specular)) * color * 0.8; 
+	
+	if( (fs_in.FragPosLightSpace.y <= -0.9) || (fs_in.FragPosLightSpace.y >= 0.9) || (fs_in.FragPosLightSpace.x >= 0.9) || (fs_in.FragPosLightSpace.x <= -0.9) ){
+		lighting = (ambient + (1.0) * (diffuse + specular)) * color * 8.0; 
+	}
+	
+	
+    FragColor = vec4(lighting, 1.0);
+}
